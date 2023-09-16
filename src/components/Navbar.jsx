7c9cff23c4638fd2f0094863c22d7e17cfd3de83
Mihajlo Kostic <mihajlo.kostic25200@gmail.com>
2023-09-16 12:46:10 +0200

    Add toggleMenu (unfinished)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import NavStyles from "./NavStyles.module.css";
 import { LuMenu } from "react-icons/lu";
 
 export const Navbar = () => {
+
+
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  const handleToggleMenu = (e) => {
+    e.preventDefault()
+    setToggleMenu(!toggleMenu);
+  }
+
   return (
-    <div className="bg-base-100 w-full min-h-16 items-center grid grid-cols-[1fr_1fr] px-32 pt-14">
+    <div className="bg-base-100 w-full min-h-16 items-center grid grid-cols-[1fr_1fr] px-32 pt-14 relative overflow-x-hidden">
       <h1
         className={`normal-case text-4xl w-max font-bold text-[#eaeaea]`}
       >
         Marko Stefanović
       </h1>
       <button
-        className="btn btn-ghost min-[800px]:hidden place-self-end p-0"
+        className="btn btn-ghost place-self-end p-0 z-10"
         type="button"
+        onClick={handleToggleMenu}
       >
         <LuMenu size={36} />
       </button>
-      <div className="flex place-self-end gap-10 max-[800px]:hidden">
+      <div className={`flex flex-col place-self-end gap-10 absolute ${NavStyles.menu}`} data-toggle={toggleMenu ? "true" : "false"}>
         <NavLink
           to="/"
           className={({ isActive, isPending }) =>
