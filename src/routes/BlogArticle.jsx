7571e96e9a80b3cbcd2bf71d18e6ee6f0be5751a
Mihajlo Kostic <mihajlo.kostic25200@gmail.com>
2023-09-20 21:38:40 +0200

    Change path for blog article

--- src/routes/BlogArticle.jsx
+++ src/routes/BlogArticle.jsx
@@ -9,7 +9,7 @@ export const BlogArticle = () => {
   const [markdown, setMarkdown] = useState("");
   const fetchMarkdown = async () => {
     try {
-      const response = await fetch(`/markdown/${post.file}`);
+      const response = await fetch(`assets/markdown/${post.file}`);
       //   console.log(await response.text());
       setMarkdown(await response.text());
     } catch (error) {
