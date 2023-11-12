570de7a594c3a0a85d28bc249e79783e9bc731d5
Mihajlo Kostic <mihajlo.kostic25200@gmail.com>
2023-11-12 14:09:11 +0100

    Changes for publish

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -40,7 +40,7 @@ export const Cards = ({ posts }) => {
                 }}
                 aria-label="Read more"
               >
-                read more
+                Read More
               </Link>
             </div>
           </div>
